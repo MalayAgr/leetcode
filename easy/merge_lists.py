94f2338698164cb9c3191b52541c9952bc977380
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-06-30 01:25:45 +0530

    Update merge_lists.py to use LinkedList

--- easy/merge_lists.py
+++ easy/merge_lists.py
@@ -1,3 +1,10 @@
+import sys
+
+sys.path.insert(0, "path/to/folder")
+
+from src.ds import LinkedList
+
+
 class ListNode:
     def __init__(self, val=0, next=None) -> None:
         self.val = val
@@ -7,18 +14,17 @@ class ListNode:
         return f"{self.__class__.__name__}(val={self.val})"
 
 
-def merge_sorted_lists(list_a: ListNode | None, list_b: ListNode | None):
-    curr_a = list_a
-    curr_b = list_b
+def merge_sorted_lists(list_a: LinkedList, list_b: LinkedList) -> LinkedList:
+    if list_a.is_empty():
+        return list_b
 
-    if curr_a is None:
-        return curr_b
+    curr_a, curr_b = list_a.head, list_b.head
 
-    result = ListNode(-1)
-    curr_res = result
+    result = LinkedList()
+    curr_res = result.push(-1)
 
     while curr_a is not None and curr_b is not None:
-        if curr_a.val <= curr_b.val:
+        if curr_a.value <= curr_b.value:
             curr_res.next = curr_a
             curr_res, curr_a = curr_res.next, curr_a.next
         else:
@@ -28,29 +34,25 @@ def merge_sorted_lists(list_a: ListNode | None, list_b: ListNode | None):
     longer = curr_b if curr_a is None else curr_a
     curr_res.next = longer
 
-    result = result.next
+    result.pop()
 
     return result
 
 
 if __name__ == "__main__":
-    l1 = ListNode(val=1)
-    temp = l1
+    l1 = LinkedList()
+    temp = l1.push(1)
 
     for i in (2, 3, 5):
-        temp.next = ListNode(val=i)
-        temp = temp.next
+        temp = l1.insert_after(temp, value=i)
 
-    l2 = ListNode(val=0)
-    temp = l2
+    l2 = LinkedList()
+    temp = l2.push(0)
 
     for i in (2, 4):
-        temp.next = ListNode(val=i)
-        temp = temp.next
+        temp = l2.insert_after(temp, value=i)
 
     result = merge_sorted_lists(list_a=l1, list_b=l2)
-    temp = result
 
-    while temp is not None:
-        print(temp.val)
-        temp = temp.next
+    for i in result:
+        print(i)
