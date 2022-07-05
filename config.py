2e969f6eb0f24eeeb7c77594ea981a6db18cd86d
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-07-05 16:25:19 +0530

    Add TOC to template

--- config.py
+++ config.py
@@ -18,6 +18,25 @@ nav_order: {{ number }}
 
 > Leetcode Link: [{{ name }}]({{ link }}).
 
+- [Statement](#statement)
+- [Examples](#examples)
+{%- if n_solutions == 1 %}
+- [Solution](#solution)
+  - [Concept](#concept)
+  - [Example](#example)
+  - [Time complexity](#time-complexity)
+{%- else %}
+- [Solution 1](#solution-1)
+  - [Concept](#concept)
+  - [Example](#example)
+  - [Time complexity](#time-complexity)
+{%- for i in (2..n_solutions) %}
+- [Solution {{ i }}](#solution-{{ i }})
+  - [Concept](#concept-{{ i | minus: 1 }})
+  - [Example](#example-{{ i | minus: 1 }})
+  - [Time complexity](#time-complexity-{{ i | minus: 1 }})
+{%- endfor %}
+{% endif %}
 ## Statement
 
 ## Examples
